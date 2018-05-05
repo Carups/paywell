efe3ddd867c5509a624fa6faa38d477fdcb047a7
Anton <anton1321@mail.ru>
2018-05-05 21:38:54 +0300

    Добавлено шифрование.
    paywell/serverKeyStore.jks -- теоретически выкладывать нельзя

--- paywell/app/src/main/java/ru/spbau/mit/hackathon/paywell/server/AppClient.java
+++ paywell/app/src/main/java/ru/spbau/mit/hackathon/paywell/server/AppClient.java
@@ -3,11 +3,23 @@ package ru.spbau.mit.hackathon.paywell.server;
 import android.os.AsyncTask;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
 
 
 public class AppClient extends AsyncTask<String, Void, String> {
@@ -16,33 +28,54 @@ public class AppClient extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... strings) {
         try {
             return getJsonFromString();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         } catch (IOException e) {
-            return "-1";
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
         }
+        return "-1";
     }
 
-    private String getJsonFromString() throws IOException {
+    private String getJsonFromString() throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException, KeyManagementException {
         final String hostName = "10.0.2.2";
         final int portNumber = 8080;
+
         String fromServer = null;
-//
-//        try (
-                Socket kkSocket = new Socket(hostName, portNumber);
+
+        //passphrase for keystore
+        char[] keystorePass="changeit".toCharArray();
+        SSLContext sslContext = null;
+
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        //keyStore.load(Resources.getSystem().openRawResource(R.raw.client_trust_store), keystorePass);
+        keyStore.load(new ByteArrayInputStream(PublicKeyHolder.certArr), keystorePass);
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
+
+        sslContext = SSLContext.getInstance("TLS");
+
+        sslContext.init(
+                null,
+                trustManagerFactory.getTrustManagers(),
+                new SecureRandom()
+        );
+
+                SSLSocket kkSocket = (SSLSocket)sslContext.getSocketFactory().createSocket(hostName, portNumber);
                 PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(kkSocket.getInputStream()));
-//        ) {
+
             fromServer = in.readLine();//System.out.println("Server: " + fromServer);
             out.println("{\"purpose\" : \"orgList\"}");
             fromServer = in.readLine();
-//        } catch (UnknownHostException e) {
-//            System.err.println("Don't know about host " + hostName);
-//            System.exit(1);
-//        } catch (IOException e) {
-//            System.err.println("Couldn't get I/O for the connection to " +
-//                    hostName);
-//            System.exit(1);
-//        }
+
         return fromServer;
     }
 }
--- paywell/app/src/main/java/ru/spbau/mit/hackathon/paywell/server/AppServer.java
+++ paywell/app/src/main/java/ru/spbau/mit/hackathon/paywell/server/AppServer.java
@@ -1,26 +1,31 @@
 package ru.spbau.mit.hackathon.paywell.server;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.ArrayList;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 
 public class AppServer {
 
     public static void main(String[] args) throws IOException {
 
-        if (args.length != 1) {
-            System.err.println("Usage: java "
-                    + AppServer.class.getSimpleName()
-                    + " <port number>");
-            System.exit(1);
-        }
+        new FileInputStream("serverKeyStore.jks");
+        System.setProperty("javax.net.ssl.keyStore", "serverKeyStore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
 
-        int portNumber = 8080; //Integer.parseInt(args[0]);
+        int portNumber = 8080;
         boolean listening = true;
 
-        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+        SSLServerSocketFactory sslSrvFact =
+                (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+
+        try (SSLServerSocket serverSocket = (SSLServerSocket)sslSrvFact.createServerSocket(portNumber)) {
             while (listening) {
                 new AppServerThread(serverSocket.accept()).start();
+                System.out.println("Connection to server");
             }
         } catch (IOException e) {
             System.err.println("Could not listen on port " + portNumber);
